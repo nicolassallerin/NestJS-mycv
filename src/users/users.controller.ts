0ba158593690b5814a73950d1949969b46cd042d
nicolassallerin <nicolas.sallerin@oclock.io>
2023-03-13 15:28:26 +0100

    add users controller methods V1

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
-@Controller('auth')
+@Controller('/auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -11,5 +24,32 @@ export class UsersController {
     const { email, password } = body;
 
     this.usersService.create(email, password);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -8,6 +8,7 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
+    // plutôt que create() car save() déclenche les hooks (et pas create())
     const user = this.repo.create({ email, password });
 
     return this.repo.save(user);
@@ -25,7 +26,7 @@ export class UsersService {
     const user = await this.repo.findOneBy({ id });
 
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
 
     Object.assign(user, attrs);
@@ -37,7 +38,7 @@ export class UsersService {
     const user = await this.repo.findOneBy({ id });
 
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
 
     return this.repo.remove(user);
